737b0740a85a6aec8c85f207a1fbfc3056a18c48
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-06-06 12:57:41 -0600

    Experimented with some different lengths, still unable to see action potential.

--- myelinated_HH.m
+++ myelinated_HH.m
@@ -6,21 +6,22 @@ clc
 % length of the axon to 0.1 for example and shrink the space step.
 
 % defining all of the initial and constant variables
-c_m = 0.001; % membrane capacitance (ms / (ohm*cm^2)) (could try nodal capacitance of 2)
-r_l = 30; % specific intracellular resistivity (or axoplasmic resistivity) (ohms * cm)
-nonmyelinated_radius = 0.000165; % (cm)
-myelinated_radius = 0.0003; % (cm)
+c_m_nodal = 0.002; % membrane capacitance (ms / (ohm*cm^2))
+c_m_internodal = 0.001; % (ms / (ohm*cm^2))
+r_l = 70; % specific intracellular resistivity (or axoplasmic resistivity) (ohms * cm)
+radius_nodal = 0.000165; % (cm)
+radius_internodal = 0.0003; % (cm)
 h = 0.0001; % space step (this)
 total_time = 35; % we only ever want to run up to 35 ms (where we find equilibrium)
 k = 0.01; % time step (MAY CHANGE LATER)
-g_L = 0.0003; % (1/(ohm*cm^2))
-nonmyelinated_g_k = 0.036; % (1/(ohm*cm^2))
-myelinated_g_k = 0; % (1/(ohm*cm^2))
-nonmyelinated_g_Na = 0.12; % (1/(ohm*cm^2))
-myelinated_g_Na = 0; % (1/(ohm*cm^2))
-E_k = -77; % (mV)
+g_L = 0.08; % (1/(ohm*cm^2))
+g_k_nodal = 0.08; % (1/(ohm*cm^2))
+g_k_internodal = 0; % (1/(ohm*cm^2))
+g_Na_nodal = 3; % (1/(ohm*cm^2))
+g_Na_internodal = 0; % (1/(ohm*cm^2))
+E_k = -84; % (mV)
 E_Na = 50; % (mV)
-E_L = -54.4; % (mV)
+E_L = -83.38; % (mV)
 
 % The following are functions of voltage
 alpha_n = @(V) 0.01*(V + 55)/(1 - exp(-(V + 55)/10));
@@ -32,10 +33,10 @@ beta_h = @(V) 1/(1 + exp(-(V + 35)/10));
 
 % defining nodal regions, and the axon length will be based on how many
 % regions we have 
-num_of_nodes = 5;
+num_of_nodes = 3;
 % the first number is in um, the *10^(-4) converts it to cm
-nodal_length = 2.3*10^(-4); % (in cm)
-myelinated_length = 85*10^(-4); % (in cm)
+nodal_length = 50*10^(-4); % (in cm)
+myelinated_length = 200*10^(-4); % (in cm)
 d = (nodal_length * num_of_nodes) + (myelinated_length * num_of_nodes); % axon length (in cm)
 
 
@@ -48,41 +49,43 @@ end
 % (the following conductances and radius are fcns of space, to mimmick myelin)
 % starting each of the functions with a condition. This condition being the
 % values of each if the position is in the first nodal region
-a = @(x) (x > nodal_regions(1, 1) && x < nodal_regions(2, 1));
-g_Na = @(x) (x > nodal_regions(1, 1) && x < nodal_regions(2, 1));
-g_k = @(x) (x > nodal_regions(1, 1) && x < nodal_regions(2, 1));
+a = @(x) (x >= nodal_regions(1, 1) && x <= nodal_regions(2, 1));
+c_m = @(x) (x >= nodal_regions(1, 1) && x <= nodal_regions(2, 1));
+g_Na = @(x) (x >= nodal_regions(1, 1) && x <= nodal_regions(2, 1));
+g_k = @(x) (x >= nodal_regions(1, 1) && x <= nodal_regions(2, 1));
 
 % creating a for loop that will turn the a, g_k and g_Na into piecewise
 % functions, having different values in nodal regions vs myelinated regions 
 for i = 2:num_of_nodes
-    a = @(x) a(x) || (x > nodal_regions(1, i) && x < nodal_regions(2, i));
-    g_Na = @(x) g_Na(x) || (x > nodal_regions(1, i) && x < nodal_regions(2, i));
-    g_k = @(x) g_k(x) || (x > nodal_regions(1, i) && x < nodal_regions(2, i));
+    a = @(x) a(x) || (x >= nodal_regions(1, i) && x <= nodal_regions(2, i));
+    c_m = @(x) c_m(x) || (x >= nodal_regions(1, i) && x <= nodal_regions(2, i));
+    g_Na = @(x) g_Na(x) || (x >= nodal_regions(1, i) && x <= nodal_regions(2, i));
+    g_k = @(x) g_k(x) || (x >= nodal_regions(1, i) && x <= nodal_regions(2, i));
 end
 
 % finally, we make a, g_Na, and g_k certain values if the conditions above
 % are met or not the first element of the summation is if x is in a nodal
 % region, the second element is if the x is in a myelinated region.
-a = @(x) nonmyelinated_radius*a(x) + myelinated_radius*(a(x)==0); % axon radius (where both values (0.0025 and 0.005) are in cm)
-g_k = @(x) nonmyelinated_g_k*g_k(x) + myelinated_g_k*(g_k(x)==0); % (g_k is in 1/(ohm*cm^2))
-g_Na = @(x) nonmyelinated_g_Na*g_Na(x) + myelinated_g_Na*(g_Na(x)==0); % (g_Na is in 1/(ohm*cm^2))
+a = @(x) radius_nodal*a(x) + radius_internodal*(a(x)==0); % axon radius (where both values (0.0025 and 0.005) are in cm)
+c_m = @(x) c_m_nodal*c_m(x) + c_m_internodal*(c_m(x)==0);
+g_k = @(x) g_k_nodal*g_k(x) + g_k_internodal*(g_k(x)==0); % (g_k is in 1/(ohm*cm^2))
+g_Na = @(x) g_Na_nodal*g_Na(x) + g_Na_internodal*(g_Na(x)==0); % (g_Na is in 1/(ohm*cm^2))
 
 
 % adding sodium conductance (stimulus)
-S = 0.1; %0.01; % (in 1/(ohm*cm^2))
+S = 5; %0.01; % (in 1/(ohm*cm^2))
 T0 = 0; % start time of when stimulus is added (in ms)
 T1 = 1; % end time of when stimulus is added (in ms)
 
 % NOTE: the stimulus MUST be added in a nodal region from (0um to 1um is fine)
 P0 = 0; % position of adding the stimulus (in cm)
-P1 = 0.0002; % ending position of adding the stimulus (in cm or *10^4 in um)
-
+P1 = 0.0050; % ending position of adding the stimulus (in cm or *10^4 in um)
 
 % INITIAL CONDITIONS
-N_0 = 0.3177; % probability that potassium gate is open (eq: 0.3177)
-M_0 = 0.0529; % probability that Sodium activation gate is open (eq: 0.0529)
-H_0 = 0.5961; % probability that Sodium inactivation gate is open (eq: 0.5961)
-V_initial = -64.9997; % (mV) Voltage (eq: -64.9997)
+N_0 = 0.1009; % probability that potassium gate is open (eq: 0.1009)
+M_0 = 0.0051; % probability that Sodium activation gate is open (eq: 0.0051)
+H_0 = 0.9571; % probability that Sodium inactivation gate is open (eq: 0.9571)
+V_initial = -83.3794; % (mV) Voltage (eq: -83.3794)
 
 % should be CAREFUL about rounding here. For some reason matlab thinks that
 % something like 3.678e3 is not considered an interger sometimes
@@ -117,26 +120,26 @@ for j = 1:(n-1)
 
         % defining coefficients
         a1 = -a(i)/(2*r_l*h^2);
-        a2 = a(i)/(r_l*h^2) + c_m/k + g_k(i)*N(1, i)^4 + g_Na(i)*M(1, i)^3*H(1, i) + g_L;
+        a2 = a(i)/(r_l*h^2) + c_m(i)/k + g_k(i)*N(1, i)^4 + g_Na(i)*M(1, i)^3*H(1, i) + g_L;
         a3 = -a(i)/(2*r_l*h^2); 
-        a4 = c_m/k; 
+        a4 = c_m(i)/k; 
         a5 = g_k(i)*N(1, i)^4*E_k + g_Na(i)*M(1, i)^3*H(1, i)*E_Na + g_L*E_L;
 
         % % adding the stimulus during a certain time interval: (T0 - T1)
         % if j*k >= T0 && j*k <= T1
-        %     a2 = a(i)/(r_l*h^2) + c_m/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
+        %     a2 = a(i)/(r_l*h^2) + c_m(i)/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
         %     a5 = g_k(i)*N(1, i)^4*E_k + (g_Na(i)*M(1, i)^3*H(1, i) + S)*E_Na + g_L*E_L;
         % end
         % 
         % % adding the stimulus at a spacial interval: (P0 - P1)
         % if i*h >= P0 && i*h <= P1 
-        %     a2 = a(i)/(r_l*h^2) + c_m/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
+        %     a2 = a(i)/(r_l*h^2) + c_m(i)/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
         %     a5 = g_k(i)*N(1, i)^4*E_k + (g_Na(i)*M(1, i)^3*H(1, i) + S)*E_Na + g_L*E_L;
         % end
 
         % % adding stimulus in specific space AND time interval:
         if (j*k >= T0 && j*k <= T1) && (i*h >= P0 && i*h <= P1)
-            a2 = a(i)/(r_l*h^2) + c_m/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
+            a2 = a(i)/(r_l*h^2) + c_m(i)/k + g_k(i)*N(1, i)^4 + (g_Na(i)*M(1, i)^3*H(1, i) + S) + g_L;
             a5 = g_k(i)*N(1, i)^4*E_k + (g_Na(i)*M(1, i)^3*H(1, i) + S)*E_Na + g_L*E_L;
         end
 
@@ -203,20 +206,14 @@ end
 % units in um)
 position1 = 0.0001; % in cm 
 position2 = 0.0002; % in cm
-position3 = 0.0003; % in cm
-position4 = 0.001; % in cm
-position5 = 0.0015; % in cm 
-position6 = 0.002; % in cm
-position7 = 0.0025; % in cm
-position8 = 0.003; % in cm
-position9 = 0.0035; % in cm 
-position10 = 0.004; % in cm
-
-position1/h
-position2/h
-position3/h
-position4/h
-
+position3 = 0.001; % in cm
+position4 = 0.005; % in cm
+position5 = 0.01; % in cm 
+position6 = 0.02; % in cm
+position7 = 0.03; % in cm
+position8 = 0.04; % in cm
+position9 = 0.05; % in cm 
+position10 = 0.06; % in cm
 
 % Times to observe the voltage along the axon
 time1 = 1; % in ms
