244d76d20756674b750cb30be7af71430e44d027
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-07-18 22:32:13 -0600

    Plotted some nice animation.

--- plotter.m
+++ plotter.m
@@ -19,6 +19,9 @@ data6 = load('dimless_stim_0.05.mat');
 data7 = load('myelin_stim_0.371.mat');
 data8 = load('myelin_stim_0.5.mat');
 data9 = load('myelin_stim_0.5_nodel_0.0005.mat')
+data10 = load('myelin_stim_0.5_length_1..mat')
+data11 = load('myelin_stim_0.5_length_1_2.mat')
+
 
 % norms of each matrix comparison
 % conclude that the matrices are very similar due to 10^-9 norm or smaller
@@ -65,80 +68,80 @@ norm(data3.Uall-data6.Uall*25)
 % PLOTTING MYELINATED AXON %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-d = data8.d;
-T = data8.T;
-m = data8.m;
-n = data8.n;
-k = data8.k;
-h = data8.h;
+d = data10.L;
+T = data10.T;
+m = data10.m;
+n = data10.n;
+k = data10.k;
+h = data10.h;
 % 
 % % SPATIAL PROFILE %
 % % x axis is the axon length
-% t1 = linspace(0, d, m); 
-% 
-% figure(1);
-% hold on;
-% 
-% xmin = 0;
-% xmax = d;
-% ymin = -90;
-% ymax = 60;
-% 
-% axis([xmin xmax ymin ymax]);  % Set axis limits
-% xlabel('Axon length in cm');
-% ylabel('Voltage of axon in mV');
-% 
-% % Loop through each vector and plot them one by one
-% for i = 1:n
-%     x1 = data8.Uall(i,:);
-% %     x2 = data2.Uall(i,:);
-%     
-%     % Plot the vector
-%     plot(t1, x1, 'b-');
-% %     hold on
-% %     plot(t1, x2, 'r-');
-%     
-%     text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Time: %.2f ms', round(i*k, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
-%     
-%     % Add a pause to create animation effect
-%     pause(0.01);
-%     
-%     cla;
-% end
+t1 = linspace(0, d, m); 
 
-% TEMPORAL PROFILE %
-% x axis is the axon time
-t2 = linspace(0, T, n); 
-
-figure(2);
+figure(1);
 hold on;
 
 xmin = 0;
-xmax = T;
+xmax = d;
 ymin = -90;
 ymax = 60;
 
 axis([xmin xmax ymin ymax]);  % Set axis limits
-xlabel('Time in ms');
+xlabel('Axon length in cm');
 ylabel('Voltage of axon in mV');
 
 % Loop through each vector and plot them one by one
-for i = 1:m
-    x1 = data8.Uall(:,i);  
-%     x2 = data8.Uall(:,i);
+for i = 1:n
+    x1 = data10.Uall(i,:);
+    x2 = data11.Uall(i,:);
     
     % Plot the vector
-    plot(t2, x1, 'b-');
+    plot(t1, x1, 'b-');
     hold on
-%     plot(t2, x2, 'r-');
+    plot(t1, x2, 'r-');
     
-    text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Space: %.2f cm', round(i*h, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
+    text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Time: %.2f ms', round(i*k, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
     
     % Add a pause to create animation effect
     pause(0.01);
     
     cla;
 end
+
+% TEMPORAL PROFILE %
+% x axis is the axon time
+% t2 = linspace(0, T, n); 
+% 
+% figure(2);
+% hold on;
+% 
+% xmin = 0;
+% xmax = T;
+% ymin = -90;
+% ymax = 60;
+% 
+% axis([xmin xmax ymin ymax]);  % Set axis limits
+% xlabel('Time in ms');
+% ylabel('Voltage of axon in mV');
+% 
+% % Loop through each vector and plot them one by one
+% for i = 1:m
+%     x1 = data10.Uall(:,i);  
+%     x2 = data11.Uall(:,i);
+%     
+%     % Plot the vector
+%     plot(t2, x1, 'b-');
+%     hold on
+%     plot(t2, x2, 'r-');
+%     
+%     text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Space: %.2f cm', round(i*h, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
+%     
+%     % Add a pause to create animation effect
+%     pause(0.001);
+%     
+%     cla;
+% end
 
 
 
@@ -156,9 +159,9 @@ end
 % n = data1.n;
 % k = data1.k;
 % h = data1.h;
-% 
-% % SPATIAL PROFILE %
-% % x axis is the axon length
+
+% SPATIAL PROFILE %
+% x axis is the axon length
 % t1 = linspace(0, d, m); 
 % 
 % figure(1);
@@ -176,12 +179,12 @@ end
 % % Loop through each vector and plot them one by one
 % for i = 1:n
 %     x1 = data1.Uall(i,:);
-%     x2 = data4.Uall(i,:);
+%     % x2 = data4.Uall(i,:);
 %     
 %     % Plot the vector
 %     plot(t1, x1, 'b-');
 %     hold on
-%     plot(t1, x2, 'r-');
+%     % plot(t1, x2, 'r-');
 %     
 %     text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Time: %.2f ms', round(i*k, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
 %     
@@ -210,17 +213,17 @@ end
 % % Loop through each vector and plot them one by one
 % for i = 1:m
 %     x1 = data1.Uall(:,i);  
-%     x2 = data2.Uall(:,i);
+% %     x2 = data2.Uall(:,i);
 %     
 %     % Plot the vector
 %     plot(t2, x1, 'b-');
 %     hold on
-%     plot(t2, x2, 'r-');
+% %     plot(t2, x2, 'r-');
 %     
 %     text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Space: %.2f cm', round(i*h, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
 %     
 %     % Add a pause to create animation effect
-%     pause(0.01);
+%     pause(0.05);
 %     
 %     cla;
 % end
@@ -234,12 +237,12 @@ end
 % % now pick a position to plot all of the voltages (multiply by 10000 to get
 % % units in um)
 % position1 = 0.5; % in cm
-% position2 = 1; % in cm
-% position3 = 1.5; % in cm
-% position4 = 2; % in cm
-% position5 = 2.5; % in cm
-% position6 = 3; % in cm
-% position7 = 3.5; % in cm
+% position2 = 0.52; % in cm
+% position3 = 0.54; % in cm
+% position4 = 0.56; % in cm
+% position5 = 0.58; % in cm
+% position6 = 0.6; % in cm
+% position7 = 0.7; % in cm
 % 
 % list_of_positions = [position1
 %                      position2
@@ -250,13 +253,13 @@ end
 %                      position7];
 % 
 % % Times to observe the voltage along the axon
-% time1 = 5; % in ms
-% time2 = 6; % in ms
-% time3 = 8; % in ms
-% time4 = 10; % in ms
-% time5 = 10.5; % in ms
-% time6 = 10.8; % in ms
-% time7 = 11; % in ms
+% time1 = 2; % in ms
+% time2 = 4; % in ms
+% time3 = 5; % in ms
+% time4 = 6; % in ms
+% time5 = 7; % in ms
+% time6 = 8; % in ms
+% time7 = 9; % in ms
 % 
 % list_of_times = [time1
 %                  time2
@@ -269,10 +272,10 @@ end
 % % plotting Voltage vs Axon length
 % figure(1)
 % t1 = linspace(0, d, m);
-% plot(t1, Uall(round(time1/k),:))
+% plot(t1, data10.Uall(round(time1/k),:))
 % for i = 2:length(list_of_times)
 %     hold on
-%     plot(t1, Uall(round(list_of_times(i)/k),:))
+%     plot(t1, data10.Uall(round(list_of_times(i)/k),:))
 % end
 % 
 % % describing plots using legends
@@ -289,10 +292,10 @@ end
 % t2 = linspace(0, T, n); % FULL MATRIX
 % % t2 = linspace(0, T, n*k*2); % MATRIX AT EVERY 50th iteration
 % % t2 = linspace(0, T, n*k); % MATRIX AT EVERY 100th iteration
-% plot(t2, Uall(:,round(position1/h)))
+% plot(t2, data10.Uall(:,round(position1/h)))
 % for i = 2:length(list_of_positions)
 %     hold on
-%     plot(t2, Uall(:,round(list_of_positions(i)/h)))
+%     plot(t2, data10.Uall(:,round(list_of_positions(i)/h)))
 % end
 % 
 % % describing plots using legends
