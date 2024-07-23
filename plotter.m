96138dffcb61c4fb6a19e22ab209baa87d455cab
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2024-07-23 13:20:38 -0600

    Added some close up animation.

--- plotter.m
+++ plotter.m
@@ -111,37 +111,57 @@ h = data10.h;
 
 % TEMPORAL PROFILE %
 % x axis is the axon time
-t2 = linspace(0, T, n); 
 
-figure(2);
-hold on;
-
-xmin = 0;
-xmax = T;
-ymin = -90;
-ymax = 60;
-
-axis([xmin xmax ymin ymax]);  % Set axis limits
-xlabel('Time in ms');
-ylabel('Voltage of axon in mV');
-
-% Loop through each vector and plot them one by one
-for i = 1:m
-    x1 = data10.Uall(:,i);  
-    x2 = data11.Uall(:,i);
-
-    % Plot the vector
-    plot(t2, x1, 'b-');
-    hold on
-    plot(t2, x2, 'r-');
-
-    text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Space: %.2f cm', round(i*h, 2)), 'FontSize', 12, 'BackgroundColor', 'w');
-
-    % Add a pause to create animation effect
-    pause(0.001);
-
-    cla;
-end
+% defining plot linspace interval
+% T1 = 5;
+% T2 = 6.2;
+% steps = (T2-T1)/k+1;
+% t2 = linspace(T1, T2, steps); 
+% 
+% figure(2);
+% hold on;
+% 
+% xmin = T1;
+% xmax = T2;
+% ymin = -26;
+% ymax = 10;
+% 
+% axis([xmin xmax ymin ymax]);  % Set axis limits
+% xlabel('Time in ms');
+% ylabel('Voltage of axon in mV');
+% 
+% % defining space interval
+% P1 = 0.4; % (in cm)
+% P2 = 0.6; % (in cm)
+% 
+% nodal_regions = data10.nodal_regions;
+% 
+% color = 'r-';
+% % Loop through each vector and plot them one by one
+% for i = P1/h:P2/h
+%     x1 = data10.Uall(T1/k:T2/k,i);  
+% %     x2 = data11.Uall(:,i);
+%     
+%     for j = 1:85
+%         if (round(i*h, 4) == round(nodal_regions(1, j), 4)) || (round(i*h,4) == round(nodal_regions(2, j), 4))
+%             color = 'b-';
+%             break;
+%         else
+%             color = 'r-';
+%         end
+%     end
+%     % Plot the vector
+%     plot(t2, x1, color, 'LineWidth', 3);
+%     hold on
+% %     plot(t2, x2, 'r-');
+% 
+%     text(xmax - 0.25 * (xmax - xmin), ymin + 0.1 * (ymax - ymin), sprintf('Space: %.4f cm', round(i*h, 4)), 'FontSize', 12, 'BackgroundColor', 'w');
+% 
+%     % Add a pause to create animation effect
+%     pause(0.05);
+% 
+%     cla;
+% end
 
 
 
@@ -234,41 +254,40 @@ end
 % PLOTTING MULTIPLE LINES %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% % now pick a position to plot all of the voltages (multiply by 10000 to get
-% % units in um)
-% position1 = 0.5; % in cm
-% position2 = 0.52; % in cm
-% position3 = 0.54; % in cm
-% position4 = 0.56; % in cm
-% position5 = 0.58; % in cm
-% position6 = 0.6; % in cm
-% position7 = 0.7; % in cm
-% 
-% list_of_positions = [position1
-%                      position2
-%                      position3
-%                      position4
-%                      position5
-%                      position6
-%                      position7];
-% 
-% % Times to observe the voltage along the axon
-% time1 = 2; % in ms
-% time2 = 4; % in ms
-% time3 = 5; % in ms
-% time4 = 6; % in ms
-% time5 = 7; % in ms
-% time6 = 8; % in ms
-% time7 = 9; % in ms
-% 
-% list_of_times = [time1
-%                  time2
-%                  time3
-%                  time4
-%                  time5
-%                  time6
-%                  time7];
-% 
+% now pick a position to plot all of the voltages (multiply by 10000 to get
+% units in um)
+position1 = 0.4075; % in cm
+position2 = 0.4085; % in cm
+position3 = 0.4555; % in cm
+position4 = 0.4565; % in cm
+position5 = 0.5035; % in cm
+position6 = 0.5045; % in cm
+position7 = 0.7; % in cm
+
+list_of_positions = [position1
+                     position2
+                     position3
+                     position4
+                     position5
+                     position6];
+
+% Times to observe the voltage along the axon
+time1 = 2; % in ms
+time2 = 4; % in ms
+time3 = 5; % in ms
+time4 = 6; % in ms
+time5 = 7; % in ms
+time6 = 8; % in ms
+time7 = 9; % in ms
+
+list_of_times = [time1
+                 time2
+                 time3
+                 time4
+                 time5
+                 time6
+                 time7];
+
 % % plotting Voltage vs Axon length
 % figure(1)
 % t1 = linspace(0, d, m);
@@ -286,42 +305,39 @@ end
 % legend(legendStrings1, 'Interpreter','latex')
 % ylabel("Voltage in millivolts.")
 % xlabel("Length of the axon in cm.")
-% 
-% % plotting Voltage vs Time
-% figure(2)
-% t2 = linspace(0, T, n); % FULL MATRIX
-% % t2 = linspace(0, T, n*k*2); % MATRIX AT EVERY 50th iteration
-% % t2 = linspace(0, T, n*k); % MATRIX AT EVERY 100th iteration
-% plot(t2, data10.Uall(:,round(position1/h)))
-% for i = 2:length(list_of_positions)
-%     hold on
-%     plot(t2, data10.Uall(:,round(list_of_positions(i)/h)))
-% end
-% 
-% % describing plots using legends
-% legendStrings2 = {};
-% for i = 1:length(list_of_positions)
-%     legendStrings2{end+1} = sprintf('Voltage at x = %g cm', list_of_positions(i));
-% end
-% legend(legendStrings2, 'Interpreter', 'latex')
-% ylabel("Voltage in millivolts.")
-% xlabel("Time in milliseconds.")
-% 
-% % plotting N, M, H probability vs time (at a certain position)
-% figure(3)
-% plot(t2, Nall(:,round(position3/h)))
-% hold on
-% plot(t2, Mall(:,round(position3/h)))
-% hold on
-% plot(t2, Hall(:,round(position3/h)))
-% legendStrings3 = {
-%     sprintf('N at x = %g cm', position3), ...
-%     sprintf('M at x = %g cm', position3), ...
-%     sprintf('H at x = %g cm', position3)};
-% legend(legendStrings3, 'Interpreter','latex')
-% ylabel("Probabilities of ion channels opening/closing.")
-% xlabel("Time in milliseconds.")
-% 
-% 
-% % save U,N,,'cable_0.01'  % this is for...
-% % save(U, M, N, H, 'test' % this )
+
+% plotting Voltage vs Time
+figure(2)
+
+t2 = linspace(0, T, n); % FULL MATRIX
+% t2 = linspace(0, T, n*k*2); % MATRIX AT EVERY 50th iteration
+% t2 = linspace(0, T, n*k); % MATRIX AT EVERY 100th iteration
+plot(t2, data10.Uall(:,round(position1/h)))
+for i = 2:length(list_of_positions)
+    hold on
+    plot(t2, data10.Uall(:,round(list_of_positions(i)/h)))
+end
+
+% describing plots using legends
+legendStrings2 = {};
+for i = 1:length(list_of_positions)
+    legendStrings2{end+1} = sprintf('Voltage at x = %g cm', list_of_positions(i));
+end
+legend(legendStrings2, 'Interpreter', 'latex')
+ylabel("Voltage in millivolts.")
+xlabel("Time in milliseconds.")
+
+% plotting N, M, H probability vs time (at a certain position)
+figure(3)
+plot(t2, Nall(:,round(position3/h)))
+hold on
+plot(t2, Mall(:,round(position3/h)))
+hold on
+plot(t2, Hall(:,round(position3/h)))
+legendStrings3 = {
+    sprintf('N at x = %g cm', position3), ...
+    sprintf('M at x = %g cm', position3), ...
+    sprintf('H at x = %g cm', position3)};
+legend(legendStrings3, 'Interpreter','latex')
+ylabel("Probabilities of ion channels opening/closing.")
+xlabel("Time in milliseconds.")
