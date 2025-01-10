0ca3dca3d98b941a789ca3db3f468c470babcbaa
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-01-10 12:35:30 -0700

    Finished all plotters for the squid axon.

--- axon_voltage_plotter.m
+++ axon_voltage_plotter.m
@@ -14,13 +14,16 @@ clc
 
 % getting the saved data
 HH_data = load('HH_data.mat');
+HH_data1 = load('HH_data1.mat');
 SC_data = load('First_SC_Code.mat');
 % DC_data = load('dc_data.mat');
 
-plot_squid_animation_temporal(HH_data);
+% plot_squid_animation_temporal(HH_data);
 % plot_squid_animation_spatial(HH_data);
+plot_squid_animation_probabilities(HH_data);
 % plot_squid_time_and_space_shots(HH_data);
-
+plot_squid_voltage_vs_time_comparison(HH_data, HH_data1);
+plot_squid_voltage_vs_space_comparison(HH_data, HH_data1);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % SQUID PLOTTER FUNCTIONS %
@@ -35,7 +38,7 @@ function plot_squid_animation_temporal(data)
 
     % % SPATIAL PROFILE %
     % % x axis is the axon length
-    t1 = linspace(0, L, m); 
+    t = linspace(0, L, m); 
 
     figure(1);
     hold on;
@@ -51,12 +54,12 @@ function plot_squid_animation_temporal(data)
 
     % Loop through each vector and plot them one by one
     for i = 1:n
-        x1 = data.Vm_all(i,:);
+        x = data.Vm_all(i,:);
         
         % Plot the vector
-        plot(t1, x1, 'b-');
+        plot(t, x, 'b-');
         
-        text(xmin + 0.1 * (xmax - xmin), ymax - 0.1 * (ymax - ymin), sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
+        text(xmin + 0.05, ymax + 0.8, sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
 
         % Add a pause to create animation effect
         pause(0.001);
@@ -74,7 +77,7 @@ function plot_squid_animation_spatial(data)
 
     % TEMPORAL PROFILE %
     % x axis is the axon time
-    t2 = linspace(0, T, n); 
+    t = linspace(0, T, n); 
 
     figure(1);
     hold on;
@@ -90,14 +93,63 @@ function plot_squid_animation_spatial(data)
 
     % Loop through each vector and plot them one by one
     for i = 1:m
-        x2 = data.Vm_all(:,i);  
+        x = data.Vm_all(:,i);  
 
         % Plot the vector
-        plot(t2, x2, 'b-');
+        plot(t, x, 'b-');
         hold on
 
-        text(xmin + 0.1 * (xmax - xmin), ymax - 0.05 * (ymax - ymin), sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
+        text(xmin + 0.2, ymax + 0.1, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
 
+        % Add a pause to create animation effect
+        pause(0.01);
+
+        cla;
+    end
+end
+
+% Animation that plots the probabilities vs time
+function plot_squid_animation_probabilities(data)
+    T = data.T;
+    m = data.m;
+    n = data.n;
+    dx = data.dx;
+
+    % TEMPORAL PROFILE %
+    % x axis is the axon time
+    t = linspace(0, T, n); 
+
+    figure(1);
+    hold on;
+
+    xmin = 0;
+    xmax = T;
+    ymin = 0;
+    ymax = 1;
+
+    axis([xmin xmax ymin ymax]);  % Set axis limits
+    xlabel('Time in milliseconds');
+    ylabel('Probabilities of gates begin open.');
+    
+    % Loop through each vector and plot them one by one
+    for i = 1:m
+        x1 = data.N_all(:,i);  
+        x2 = data.M_all(:,i);  
+        x3 = data.H_all(:,i);  
+
+        % Plot the vector
+        p1 = plot(t, x1, 'b-');
+        hold on
+        p2 = plot(t, x2, 'r-');
+        hold on
+        p3 = plot(t, x3, 'g-');
+        hold on
+        
+        % Add the legend
+        legend([p1, p2, p3], 'K activation gate', 'Na activation gate', 'Na inactivation date', 'Location', 'northeast');
+
+        text(xmin, ymax + 0.05, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
+        
         % Add a pause to create animation effect
         pause(0.01);
 
@@ -201,8 +253,96 @@ function plot_squid_time_and_space_shots(data)
 end
 
 % Plots animation of axon voltage in two different experiments
-function plot_squid_comparison(HH_data, HH_data1)
+function plot_squid_voltage_vs_time_comparison(data1, data2)
+
+    T = data1.T;
+    m = data1.m;
+    n = data1.n;
+    dx = data1.dx;
+
+    % TEMPORAL PROFILE %
+    % x axis is the axon time
+    t = linspace(0, T, n); 
+
+    figure(1);
+    hold on;
+
+    xmin = 0;
+    xmax = T;
+    ymin = -90;
+    ymax = 60;
+
+    axis([xmin xmax ymin ymax]);  % Set axis limits
+    xlabel('Time in milliseconds');
+    ylabel('$V_m$ in millivolts', 'Interpreter','latex');
+
+    % Loop through each vector and plot them one by one
+    for i = 1:m
+        x1 = data1.Vm_all(:,i);  
+        x2 = data2.Vm_all(:,i);
+
+        % Plot the vector
+        p1 = plot(t, x1, 'b-');
+        hold on
+        p2 = plot(t, x2, 'r-');
+        hold on
+        
+        % Add the legend
+        legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
+        text(xmin + 0.2, ymax + 0.1, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
+
+        % Add a pause to create animation effect
+        pause(0.01);
+
+        cla;
+    end
 end
+
+function plot_squid_voltage_vs_space_comparison(data1, data2)
+    
+    L = data1.L;
+    m = data1.m;
+    n = data1.n;
+    dt = data1.dt;
+
+    % TEMPORAL PROFILE %
+    % x axis is the axon time
+    t = linspace(0, L, m); 
+
+    figure(1);
+    hold on;
+
+    xmin = 0;
+    xmax = L;
+    ymin = -90;
+    ymax = 60;
+
+    axis([xmin xmax ymin ymax]);  % Set axis limits
+    xlabel('Length of axon in cm');
+    ylabel('$V_m$ in millivolts', 'Interpreter','latex');
+
+    % Loop through each vector and plot them one by one
+    for i = 1:n
+        x1 = data1.Vm_all(i,:);  
+        x2 = data2.Vm_all(i,:);
+
+        % Plot the vector
+        p1 = plot(t, x1, 'b-');
+        hold on
+        p2 = plot(t, x2, 'r-');
+        hold on
+        
+        % Add the legend
+        legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
+        text(xmin + 0.2, ymax + 0.1, sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
+
+        % Add a pause to create animation effect
+        pause(0.01);
+
+        cla;
+    end
+end
+
 
 
 function plot_rat_animation(SC_data)
