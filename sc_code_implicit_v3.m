92088ba27d0b3597c8ae51ce383a9ecaee4cf798
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-01-19 17:48:35 -0700

    Adding two other versions of solving the SC model.

---
+++ sc_code_implicit_v3.m
@@ -0,0 +1,5 @@
+% Solving the Single Cable Model Using Finite Difference Method Version 1
+% In this code, we represent the RHS of the first equation of the sc model 
+% as Vm_i^(j+1) and the RHS of the second equation of the sc model as Vmy_i^(j+1) 
+% Kevin Roberts
+% November 2024
