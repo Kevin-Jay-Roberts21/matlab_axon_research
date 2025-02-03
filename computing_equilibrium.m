575ce11e2975689e6ef37760d708fd804f06c981
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-02-02 18:45:27 -0700

    Added explicit Newton's method to solve for equilibrium.

--- computing_equilibrium.m
+++ computing_equilibrium.m
@@ -16,10 +16,44 @@ E_K = -77; % Equilibrium Potential for Potassium Ions (mV)
 E_Na = 50; % Equilibrium Potential for Sodium Ions (mV)
 E_L = -54.4; % Equilibrium Potential for Leak Channels (mV)
 
+% % solving for the first derivative of f1 (these functions will only be used for viewing, not used otherwise in code)
+% f1(Vm) = G_K * (0.01*(Vm + 55)/(1 - exp(-(Vm + 55)/10))/(0.01*(Vm + 55)/(1 - exp(-(Vm + 55)/10)) + 0.125*exp(-(Vm + 65)/80)))^4 * (Vm - E_K) ...
+%     + G_Na * (0.1*(Vm + 40)/(1 - exp(-(Vm + 40)/10))/(0.1*(Vm + 40)/(1 - exp(-(Vm + 40)/10)) + 4*exp(-(Vm + 65)/18)))^3 * ...
+%     (0.07*exp(-(Vm + 65)/20)/(0.07*exp(-(Vm + 65)/20) + 1/(1 + exp(-(Vm + 35)/10)))) * (Vm - E_Na) + G_L * (Vm - E_L);
+% 
+% df1(Vm) = diff(f1, Vm, 1); % the first derivative of f1(Vm) w.r.t. Vm
+
+
+%%
 % SOLVING USING NEWTONS METHOD
 
+% initial V_m guess
+V_m0 = -100; % (mV)
+V_m = V_m0;
 
+% Preallocate array to store Vm values
+Vm_values = zeros(1, 100);
 
+for i = 1:100
+
+    newVm = V_m - f2(V_m, G_K, G_Na, G_L, E_K, E_Na, E_L)/df2(V_m, G_K, G_Na, G_L, E_K, E_Na, E_L);
+    
+    % Store the new Vm value
+    Vm_values(i) = newVm;
+
+    % update
+    V_m = newVm;
+
+     % Plot every 100th Vm value to avoid overloading the plot
+    plot(1:i, Vm_values(1:i));
+    xlabel('Iteration');
+    ylabel('V_m (mV)');
+        
+end
+
+final_Vm = newVm
+
+%%
 % SOLVING USING MATLAB's FSOLVE FUNCTION
 
 % Define the function f(Vm)
@@ -53,3 +87,18 @@ n_infty(Vm_solution)
 m_infty(Vm_solution)
 h_infty(Vm_solution)
 
+
+
+
+function f_of_Vm = f2(Vm, G_K, G_Na, G_L, E_K, E_Na, E_L)
+
+    f_of_Vm = G_K * (0.01*(Vm + 55)/(1 - exp(-(Vm + 55)/10))/(0.01*(Vm + 55)/(1 - exp(-(Vm + 55)/10)) + 0.125*exp(-(Vm + 65)/80)))^4 * (Vm - E_K) ...
+    + G_Na * (0.1*(Vm + 40)/(1 - exp(-(Vm + 40)/10))/(0.1*(Vm + 40)/(1 - exp(-(Vm + 40)/10)) + 4*exp(-(Vm + 65)/18)))^3 * ...
+    (0.07*exp(-(Vm + 65)/20)/(0.07*exp(-(Vm + 65)/20) + 1/(1 + exp(-(Vm + 35)/10)))) * (Vm - E_Na) + G_L * (Vm - E_L);
+end
+
+function derivative_of_f_of_Vm = df2(Vm, G_K, G_Na, G_L, E_K, E_Na, E_L)
+
+    % computed using matlab (this is the result of the computed df1 function)
+    derivative_of_f_of_Vm = (36*(Vm/100 + 11/20)^4)/((exp(- Vm/10 - 11/2) - 1)^4*(exp(- Vm/80 - 13/16)/8 - (Vm/100 + 11/20)/(exp(- Vm/10 - 11/2) - 1))^4) + (36*(Vm/100 + 11/20)^3*(Vm + 77))/(25*(exp(- Vm/10 - 11/2) - 1)^4*(exp(- Vm/80 - 13/16)/8 - (Vm/100 + 11/20)/(exp(- Vm/10 - 11/2) - 1))^4) + (144*(Vm/100 + 11/20)^4*(Vm + 77)*(exp(- Vm/80 - 13/16)/640 + 1/(100*(exp(- Vm/10 - 11/2) - 1)) + (exp(- Vm/10 - 11/2)*(Vm/100 + 11/20))/(10*(exp(- Vm/10 - 11/2) - 1)^2)))/((exp(- Vm/10 - 11/2) - 1)^4*(exp(- Vm/80 - 13/16)/8 - (Vm/100 + 11/20)/(exp(- Vm/10 - 11/2) - 1))^5) + (72*exp(- Vm/10 - 11/2)*(Vm/100 + 11/20)^4*(Vm + 77))/(5*(exp(- Vm/10 - 11/2) - 1)^5*(exp(- Vm/80 - 13/16)/8 - (Vm/100 + 11/20)/(exp(- Vm/10 - 11/2) - 1))^4) - (42*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^3)/(5*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))*(exp(- Vm/10 - 4) - 1)^3*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^3) - (63*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^2*(Vm - 50))/(25*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))*(exp(- Vm/10 - 4) - 1)^3*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^3) + (21*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^3*(Vm - 50))/(50*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))*(exp(- Vm/10 - 4) - 1)^3*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^3) - (126*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^3*(Vm - 50)*((2*exp(- Vm/18 - 65/18))/9 + 1/(10*(exp(- Vm/10 - 4) - 1)) + (exp(- Vm/10 - 4)*(Vm/10 + 4))/(10*(exp(- Vm/10 - 4) - 1)^2)))/(5*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))*(exp(- Vm/10 - 4) - 1)^3*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^4) - (42*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^3*((7*exp(- Vm/20 - 13/4))/2000 - exp(- Vm/10 - 7/2)/(10*(exp(- Vm/10 - 7/2) + 1)^2))*(Vm - 50))/(5*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))^2*(exp(- Vm/10 - 4) - 1)^3*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^3) - (63*exp(- Vm/10 - 4)*exp(- Vm/20 - 13/4)*(Vm/10 + 4)^3*(Vm - 50))/(25*((7*exp(- Vm/20 - 13/4))/100 + 1/(exp(- Vm/10 - 7/2) + 1))*(exp(- Vm/10 - 4) - 1)^4*(4*exp(- Vm/18 - 65/18) - (Vm/10 + 4)/(exp(- Vm/10 - 4) - 1))^3) + 3/10;
+end
