0f659ceefd6b52754200b19047755e766741b6a9
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-02-05 14:03:06 -0700

    Modified code so that it runs for SC and HH experiments. Need to figure out why time and space increment legend is shifted differently for SC and HH experiments.

--- axon_voltage_plotter.m
+++ axon_voltage_plotter.m
@@ -1,6 +1,17 @@
 % Axon voltage plotter. This code is meant to plot data from the following
-% files: squid_giant_axon.m, myelianted_HH_axon.m,
-% dimensionless_squid_giant_axon.m, and sc_code_implicit.m
+% files: 
+% squid_giant_axon.m 
+% myelianted_HH_axon.m,
+% dimensionless_squid_giant_axon.m 
+% sc_code_implicit_v1.m
+% sc_code_implicit_v2.m
+% sc_code_implicit_v3.m
+% dc_code_implicit.m
+% 
+% Additionally, variables like list_of_positions, list_of_times, and pause 
+% may have to be modified depending on HH, SC or DC to obtain useful animations
+% and/or plots.
+% 
 % Kevin Roberts
 % January 2025
 
@@ -13,34 +24,70 @@ clc
 % getting the saved data
 HH_data = load('HH_data.mat');
 HH_data1 = load('HH_data1.mat');
-SC_data = load('First_SC_Code.mat');
-SC_dataT200ms = load('SC_data_T200ms.mat');
-% DC_data = load('dc_data.mat');
-
-% plot_squid_animation_voltage_vs_time(HH_data);
-% plot_squid_animation_voltage_vs_space(HH_data);
-% plot_squid_animation_probabilities(HH_data);
-% plot_squid_time_and_space_shots(HH_data);
-% plot_squid_voltage_vs_time_comparison(HH_data, HH_data1);
-% plot_squid_voltage_vs_space_comparison(HH_data, HH_data1);
-
-% plot_rat_animation_voltage_vs_time(SC_dataT200ms);
-% plot_rat_animation_voltage_vs_space(SC_dataT200ms);
-% plot_rat_animation_probabilities(SC_data); % (NOT WORKING)
-% plot_rat_time_and_space_shots(SC_dataT200ms); 
-% plot_rat_voltage_vs_time_comparison(SC_data, SC_data1); % (NOT WORKING)
-% plot_rat_voltage_vs_space_comparison(SC_data, SC_data1); % (NOT WORKING)
+SC_data = load('SC_data.mat');
+SC_data1 = load('SC_data1.mat');
 
 
+% picking time shots
+time1 = 5; % in ms
+time2 = 6; % in ms
+time3 = 8; % in ms
+time4 = 10; % in ms
+time5 = 11; % in ms
+time6 = 12; % in ms
+time7 = 13; % in ms
+
+list_of_times = [time1
+                 time2
+                 time3
+                 time4
+                 time5
+                 time6
+                 time7];
+
+
+% picking space shots
+position1 = 0.5; % in cm
+position2 = 1; % in cm
+position3 = 1.5; % in cm
+position4 = 2; % in cm
+position5 = 2.5; % in cm
+position6 = 3; % in cm
+position7 = 3.5; % in cm
+
+list_of_positions = [position1
+                     position2
+                     position3
+                     position4
+                     position5
+                     position6
+                     position7];
+
+
+% picking pause (this controls the speed of the animation, the pause variable 
+% is in seconds. It is how many seconds each time (or space) shot will be
+% paused at). NOTE: the legends are what slows down the animations, may
+% condsider getting rid of or adding them in certain cases.
+p = 0.001;
+
+
+% plot_animation_voltage_vs_time(SC_data, p);
+% plot_animation_voltage_vs_space(SC_data, p);
+% plot_animation_probabilities(SC_data, p);
+% plot_time_and_space_shots(HH_data, list_of_positions, list_of_times);
+% plot_voltage_vs_time_comparison(SC_data, SC_data1, p);
+% plot_voltage_vs_space_comparison(SC_data, SC_data1, p);
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SQUID PLOTTER FUNCTIONS %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%%%%%%%%%%%
+% PLOTTER FUNCTIONS %
+%%%%%%%%%%%%%%%%%%%%%
 
 % Animation that plots the voltage vs space
-function plot_squid_animation_voltage_vs_space(data)
+function plot_animation_voltage_vs_space(data, p)
     L = data.L;
     m = data.m;
     n = data.n;
@@ -72,14 +119,14 @@ function plot_squid_animation_voltage_vs_space(data)
         text(xmin + 0.05, ymax + 0.8, sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
 
         % Add a pause to create animation effect
-        pause(0.001);
+        pause(p);
 
         cla;
     end
 end
 
 % Animation that plots the voltage vs time
-function plot_squid_animation_voltage_vs_time(data)
+function plot_animation_voltage_vs_time(data, p)
     T = data.T;
     m = data.m;
     n = data.n;
@@ -112,14 +159,14 @@ function plot_squid_animation_voltage_vs_time(data)
         text(xmin + 0.2, ymax + 0.1, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
 
         % Add a pause to create animation effect
-        pause(0.01);
+        pause(p);
 
         cla;
     end
 end
 
 % Animation that plots the probabilities vs time
-function plot_squid_animation_probabilities(data)
+function plot_animation_probabilities(data, p)
     T = data.T;
     m = data.m;
     n = data.n;
@@ -155,20 +202,20 @@ function plot_squid_animation_probabilities(data)
         p3 = plot(t, x3, 'g-');
         hold on
         
-        % Add the legend
+        % Add the legend (NOTE: legends are what slows down animation)
         legend([p1, p2, p3], 'K activation gate', 'Na activation gate', 'Na inactivation date', 'Location', 'northeast');
 
         text(xmin, ymax + 0.05, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
         
         % Add a pause to create animation effect
-        pause(0.01);
+        pause(p);
 
         cla;
     end
 end
 
 % plotting multiple time and space shots of voltage for squid
-function plot_squid_time_and_space_shots(data)
+function plot_time_and_space_shots(data, list_of_positions, list_of_times)
 
     L = data.L;
     T = data.T;
@@ -176,47 +223,13 @@ function plot_squid_time_and_space_shots(data)
     n = data.n;
     dt = data.dt;
     dx = data.dx;
-
-    position1 = 0.5; % in cm
-    position2 = 1; % in cm
-    position3 = 1.5; % in cm
-    position4 = 2; % in cm
-    position5 = 2.5; % in cm
-    position6 = 3; % in cm
-    position7 = 3.5; % in cm
-    
-    list_of_positions = [position1
-                         position2
-                         position3
-                         position4
-                         position5
-                         position6
-                         position7];
-    
-    % Times to observe the voltage along the axon
-    time1 = 5; % in ms
-    time2 = 6; % in ms
-    time3 = 8; % in ms
-    time4 = 10; % in ms
-    time5 = 11; % in ms
-    time6 = 12; % in ms
-    time7 = 13; % in ms
-    
-    list_of_times = [time1
-                     time2
-                     time3
-                     time4
-                     time5
-                     time6
-                     time7];
     
     % plotting Voltage vs Axon length
     figure(1)
     t1 = linspace(0, L, m);
-    plot(t1, data.Vm_all(round(time1/dt),:))
-    for i = 2:length(list_of_times)
-        hold on
+    for i = 1:length(list_of_times)
         plot(t1, data.Vm_all(round(list_of_times(i)/dt),:))
+        hold on
     end
     
     % describing plots using legends
@@ -231,10 +244,9 @@ function plot_squid_time_and_space_shots(data)
     % plotting Voltage vs Time
     figure(2)
     t2 = linspace(0, T, n); % FULL MATRIX
-    plot(t2, data.Vm_all(:,round(position1/dx)))
-    for i = 2:length(list_of_positions)
-        hold on
+    for i = 1:length(list_of_positions)
         plot(t2, data.Vm_all(:,round(list_of_positions(i)/dx)))
+        hold on
     end
     
     % describing plots using legends
@@ -246,24 +258,24 @@ function plot_squid_time_and_space_shots(data)
     ylabel('$V_m$ in millivolts.', 'Interpreter', 'latex')
     xlabel("Time in milliseconds.")
     
-    % plotting N, M, H probability vs time (at a certain position)
+    % plotting N, M, H probability vs time (at the first position list_of_positions(1))
     figure(3)
-    plot(t2, data.N_all(:,round(position3/dx)))
+    plot(t2, data.N_all(:,round(list_of_positions(1)/dx)))
     hold on
-    plot(t2, data.M_all(:,round(position3/dx)))
+    plot(t2, data.M_all(:,round(list_of_positions(1)/dx)))
     hold on
-    plot(t2, data.H_all(:,round(position3/dx)))
+    plot(t2, data.H_all(:,round(list_of_positions(1)/dx)))
     legendStrings3 = {
-        sprintf('n at x = %g cm', position3), ...
-        sprintf('m at x = %g cm', position3), ...
-        sprintf('h at x = %g cm', position3)};
+        sprintf('n at x = %g cm', list_of_positions(1)), ...
+        sprintf('m at x = %g cm', list_of_positions(1)), ...
+        sprintf('h at x = %g cm', list_of_positions(1))};
     legend(legendStrings3, 'Interpreter','latex')
     ylabel("Probabilities of ion channels opening/closing.")
     xlabel("Time in milliseconds.")
 end
 
 % Plots animation of axon voltage in two different experiments
-function plot_squid_voltage_vs_time_comparison(data1, data2)
+function plot_voltage_vs_time_comparison(data1, data2, p)
 
     T = data1.T;
     m = data1.m;
@@ -297,18 +309,18 @@ function plot_squid_voltage_vs_time_comparison(data1, data2)
         p2 = plot(t, x2, 'r-');
         hold on
         
-        % Add the legend
-        legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
+        % Add the legend (NOTE: legends are what slows down animation)
+        % legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
         text(xmin + 0.2, ymax + 0.1, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
 
         % Add a pause to create animation effect
-        pause(0.01);
+        pause(p);
 
         cla;
     end
 end
 
-function plot_squid_voltage_vs_space_comparison(data1, data2)
+function plot_voltage_vs_space_comparison(data1, data2, p)
     
     L = data1.L;
     m = data1.m;
@@ -330,7 +342,7 @@ function plot_squid_voltage_vs_space_comparison(data1, data2)
     axis([xmin xmax ymin ymax]);  % Set axis limits
     xlabel('Length of axon in cm');
     ylabel('$V_m$ in millivolts', 'Interpreter','latex');
-
+    
     % Loop through each vector and plot them one by one
     for i = 1:n
         x1 = data1.Vm_all(i,:);  
@@ -342,12 +354,12 @@ function plot_squid_voltage_vs_space_comparison(data1, data2)
         p2 = plot(t, x2, 'r-');
         hold on
         
-        % Add the legend
-        legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
+        % Add the legend (NOTE: the legend is what is slowing down the animation)
+        % legend([p1, p2], 'data1 voltage', 'data2 voltage', 'Location', 'northeast');
         text(xmin + 0.2, ymax + 0.1, sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
 
         % Add a pause to create animation effect
-        pause(0.01);
+        pause(p);
 
         cla;
     end
@@ -355,275 +367,3 @@ end
 
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% SC/DC PLOTTER FUNCTIONS %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% plotting rat voltage vs space animation
-function plot_rat_animation_voltage_vs_space(data)
-    
-    L = data.L;
-    m = data.m;
-    n = data.n;
-    dt = data.dt;
-
-    % SPATIAL PROFILE %
-    % x axis is the axon length
-    t = linspace(0, L, m); 
-
-    figure(1);
-    hold on;
-
-    xmin = 0;
-    xmax = L;
-    ymin = -90;
-    ymax = 90;
-
-    axis([xmin xmax ymin ymax]);  % Set axis limits
-    xlabel('Axon length in cm');
-    ylabel('$V_m$ in millivolts.', 'Interpreter', 'latex')
-
-    % Loop through each vector and plot them one by one
-    for i = 1:n
-        x = data.Vm_all(i,:);
-        
-        % Plot the vector
-        plot(t, x, 'b-');
-        
-        text(xmin + 0.05, ymax + 0.8, sprintf('Time: %.3f ms', round(i*dt, 3)), 'FontSize', 12, 'BackgroundColor', 'w');
-
-        % Add a pause to create animation effect
-        pause(0.001);
-
-        cla;
-    end
-end
-
-% plotting rat voltage vs space animation
-function plot_rat_animation_voltage_vs_time(data)
-
-    T = data.T;
-    m = data.m;
-    n = data.n;
-    dx = data.dx;
-
-    % TEMPORAL PROFILE %
-    % x axis is the axon time
-    t = linspace(0, T, n); 
-
-    figure(1);
-    hold on;
-
-    xmin = 0;
-    xmax = T;
-    ymin = -90;
-    ymax = 60;
-
-    axis([xmin xmax ymin ymax]);  % Set axis limits
-    xlabel('Time in milliseconds');
-    ylabel('$V_m$ in millivolts', 'Interpreter','latex');
-
-    % Loop through each vector and plot them one by one
-    for i = 1:m
-        x = data.Vm_all(:,i);  
-
-        % Plot the vector
-        plot(t, x, 'b-');
-        hold on
-
-        text(xmin + 0.2, ymax + 0.1, sprintf('Space: %.5f cm', round(i*dx, 5)), 'FontSize', 12, 'BackgroundColor', 'w');
-
-        % Add a pause to create animation effect
-        pause(0.01);
-
-        cla;
-    end
-end
-
-% plotting time and space shots for the rat axon
-function plot_rat_time_and_space_shots(data)
-    
-    L = data.L;
-    T = data.T;
-    m = data.m;
-    n = data.n; 
-    dt = data.dt;
-    dx = data.dx;
-
-    position1 = L*0.25; % in cm
-    position2 = L*0.5; % in cm
-    position3 = L*0.75; % in cm
-    position4 = L; % in cm
-    
-    list_of_positions = [position1
-                         position2
-                         position3
-                         position4];
-    
-    % Times to observe the voltage along the axon
-    time1 = T*0.25; % in ms
-    time2 = T*0.5; % in ms
-    time3 = T*0.75; % in ms
-    time4 = T; % in ms
-    
-    
-    list_of_times = [time1
-                     time2
-                     time3
-                     time4
-                     ];
-    
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % PLOTTING Vm SPATIAL AND TEMPORAL PROFILES %
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % First figure: Voltage along the axon at different times
-    figure(1);
-    
-    % Adjust the figure size (Position [left, bottom, width, height])
-    set(gcf, 'Position', [100, 100, 1200, 500]); % Increase width (1200)
-    
-    % Create subplot (1 row, 2 columns, 1st subplot)
-    subplot(1, 2, 1);
-    t1 = linspace(0, 10000*L, m);
-    plot(t1, data.Vm_all(round(time1/dt),:));
-    hold on
-    for i = 2:length(list_of_times)
-        plot(t1, data.Vm_all(round(list_of_times(i)/dt),:));
-    end
-    
-    % Describing plots using legends
-    legendStrings1 = {};
-    for i  = 1:length(list_of_times)
-        legendStrings1{end+1} = sprintf('$V_m$ at t = %g ms', list_of_times(i));
-    end
-    legend(legendStrings1, 'Interpreter','latex');
-    ylabel("$V_m$ in millivolts.", 'Interpreter','latex');
-    xlabel("Length of the axon in um.");
-    
-    
-    % Second figure: Voltage vs Time at different positions
-    % Create subplot (1 row, 2 columns, 2nd subplot)
-    subplot(1, 2, 2);
-    t2 = linspace(0, T, n); % FULL MATRIX
-    plot(t2, data.Vm_all(:,round(position1/dx)));
-    hold on
-    for i = 2:length(list_of_positions)
-        plot(t2, data.Vm_all(:,round(list_of_positions(i)/dx)));
-    end
-    
-    % Describing plots using legends
-    legendStrings2 = {};
-    for i = 1:length(list_of_positions)
-        legendStrings2{end+1} = sprintf('$V_m$ at x = %g um', 10000*list_of_positions(i));
-    end
-    legend(legendStrings2, 'Interpreter', 'latex');
-    ylabel("$V_m$ in millivolts.", 'Interpreter', 'latex');
-    xlabel("Time in milliseconds.");
-    
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % PLOTTING Vmy SPATIAL AND TEMPORAL PROFILES %
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % First figure: Voltage along the axon at different times
-    figure(2);
-    
-    % Adjust the figure size (Position [left, bottom, width, height])
-    set(gcf, 'Position', [100, 100, 1200, 500]); % Increase width (1200)
-    
-    % Create subplot (1 row, 2 columns, 1st subplot)
-    subplot(1, 2, 1);
-    t1 = linspace(0, L*10000, m);
-    plot(t1, data.Vmy_all(round(time1/dt),:));
-    hold on
-    for i = 2:length(list_of_times)
-        plot(t1, data.Vmy_all(round(list_of_times(i)/dt),:));
-    end
-    
-    % Describing plots using legends
-    legendStrings1 = {};
-    for i  = 1:length(list_of_times)
-        legendStrings1{end+1} = sprintf('$V_{my}$ at t = %g ms', list_of_times(i));
-    end
-    legend(legendStrings1, 'Interpreter','latex');
-    ylabel("$V_{my}$ in millivolts.", 'Interpreter','latex');
-    xlabel("Length of the axon in um.");
-    
-    
-    % Second figure: Voltage vs Time at different positions
-    % Create subplot (1 row, 2 columns, 2nd subplot)
-    subplot(1, 2, 2);
-    t2 = linspace(0, T, n); % FULL MATRIX
-    plot(t2, data.Vmy_all(:,round(position1/dx)));
-    hold on
-    for i = 2:length(list_of_positions)
-        plot(t2, data.Vmy_all(:,round(list_of_positions(i)/dx)));
-    end
-    
-    % Describing plots using legends
-    legendStrings2 = {};
-    for i = 1:length(list_of_positions)
-        legendStrings2{end+1} = sprintf('$V_{my}$ at x = %g um', 10000*list_of_positions(i));
-    end
-    legend(legendStrings2, 'Interpreter', 'latex');
-    ylabel("$V_{my}$ in millivolts.", 'Interpreter', 'latex');
-    xlabel("Time in milliseconds.");
-    
-    
-    
-    
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % PLOTTING Vm-Vmy SPATIAL AND TEMPORAL PROFILES %
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % First figure: Voltage along the axon at different times
-    figure(3);
-    
-    % Adjust the figure size (Position [left, bottom, width, height])
-    set(gcf, 'Position', [100, 100, 1200, 500]); % Increase width (1200)
-    
-    % Create subplot (1 row, 2 columns, 1st subplot)
-    subplot(1, 2, 1);
-    t1 = linspace(0, 10000*L, m);
-    plot(t1, data.Vm_minus_Vmy(round(time1/dt),:));
-    hold on
-    for i = 2:length(list_of_times)
-        plot(t1, data.Vm_minus_Vmy(round(list_of_times(i)/dt),:));
-    end
-    
-    % Describing plots using legends
-    legendStrings1 = {};
-    for i  = 1:length(list_of_times)
-        legendStrings1{end+1} = sprintf('$V_m - V_{my}$ at t = %g ms', list_of_times(i));
-    end
-    legend(legendStrings1, 'Interpreter','latex');
-    ylabel("$V_m - V_{my}$ in millivolts.", 'Interpreter','latex');
-    xlabel("Length of the axon in um.");
-    
-    
-    % Second figure: Voltage vs Time at different positions
-    % Create subplot (1 row, 2 columns, 2nd subplot)
-    subplot(1, 2, 2);
-    t2 = linspace(0, T, n); % FULL MATRIX
-    plot(t2, data.Vm_minus_Vmy(:,round(position1/dx)));
-    hold on
-    for i = 2:length(list_of_positions)
-        plot(t2, data.Vm_minus_Vmy(:,round(list_of_positions(i)/dx)));
-    end
-    
-    % Describing plots using legends
-    legendStrings2 = {};
-    for i = 1:length(list_of_positions)
-        legendStrings2{end+1} = sprintf('$V_m - V_{my}$ at x = %g um', 10000*list_of_positions(i));
-    end
-    legend(legendStrings2, 'Interpreter', 'latex');
-    ylabel("$V_m - V_{my}$ in millivolts.", 'Interpreter', 'latex');
-    xlabel("Time in milliseconds.");
-    
-end
-
-
-
-function plot_rat_comparison(SC_data, SC_data1)
-end
