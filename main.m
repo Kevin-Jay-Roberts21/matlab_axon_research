86a532fdd9464ad7a5f6f4783f31f2a2e9474d7a
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-04-29 17:40:02 -0600

    Tested more functions.

--- main.m
+++ main.m
@@ -10,13 +10,45 @@ clc
 % Running an HH Simulation %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% % Getting the mesh and material parameters
+% hh_mesh = hh_mesh_parameters();
+% hh_material = hh_material_parameters();
+% 
+% % Running the HH simulation
+% hh_simultion = hh_function_v1(hh_mesh, hh_material);
+% 
+% % % Saving HH data
+% % save('hh_simulation.mat', 'hh_simultion');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Running a SC Simulation %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % Getting the mesh and material parameters
-hh_mesh = hh_mesh_parameters();
-hh_material = hh_material_parameters();
+sc_mesh = sc_dc_mesh_parameters();
+sc_material = set1_parameters();
 
-% Running the HH simulation
-hh_simultion = hh_function_v1(hh_mesh, hh_material);
+% Running the SC simulation
+sc_simulation = sc_function_v2(sc_mesh, sc_material);
 
-% % Saving HH data
-% save('hh_simulation.mat', 'hh_simultion');
+% % Saving SC data
+% save('sc_simulation.mat', 'sc_simultion');
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Running a DC Simulation %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% % Getting the mesh and material parameters
+% dc_mesh = sc_dc_mesh_parameters();
+% dc_material = set1_parameters();
+% 
+% % Running the SC simulation
+% dc_simulation = dc_function_v3(dc_mesh, dc_material);
+% 
+% % % Saving SC data
+% % save('sc_simulation.mat', 'sc_simultion');
+
+
 
