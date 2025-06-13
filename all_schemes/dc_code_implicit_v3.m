bf1205e4bc67f80070392e7ce0cdd32be17a5671
Kevin-Jay-Roberts21 <kevinjrob9@gmail.com>
2025-06-13 08:39:54 -0600

    testing more stimulus values.

--- all_schemes/dc_code_implicit_v3.m
+++ all_schemes/dc_code_implicit_v3.m
@@ -28,15 +28,15 @@ n = T/dt + 1; % (#) n is the number of time steps
 % Defining the material properties on other intrinsic parameters
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 a = 0.0001; % 0.55*10^(-4); % (cm) axon radius in nodal region
-a_my = 0.00014; % a/0.698; % (cm) axon radius in myelinated section 
+a_my = 0.0001238; % a/0.698; % (cm) axon radius in myelinated section 
 R_i = 0.0712; % (kilo-ohms*cm) intracellular resistivity
 R_m = 24.8; % (kilo-ohms*cm^2) specific membrane resistance
 C_m = 1.23; % (micro-farads/cm^2) specific membrane capacitance
 r_pa = 96.3*10^6; % (kilo-ohms/cm) periaxonal resistivity per unit length
 R_pa = r_pa*pi*d_pa*(2*a + d_pa); % (kilo-ohms*cm) resistivity of the periaxonal space (computed)
-r_pn = 32100*10^6; % (kilo-ohms/cm) paranodal resitance per unit length (used in BC since r_bar_pn = r_pn * L_pn) 
-R_my = 5.2779*10^10; % 63.7; % (kilo-ohms*cm^2) specific myelin resistance
-C_my = 0.0174; % 0.113; % (micro-farads/cm^2) specific myelin capacitance
+r_pn = 3210*10^6; % (kilo-ohms/cm) paranodal resitance per unit length (used in BC since r_bar_pn = r_pn * L_pn) 
+R_my = 123.6795; % 63.7; % (kilo-ohms*cm^2) specific myelin resistance
+C_my = 0.0081; % 0.113; % (micro-farads/cm^2) specific myelin capacitance
 G_K = 80; % (mS/cm^2) max specific potassium conductance
 G_Na = 3000; % (mS/cm^2) max specific sodium conductance 
 G_L = 80; % (mS/cm^2) specific leak conductance
@@ -55,9 +55,9 @@ w3 = r_pa/(r_pn*L_pn);
 %%%%%%%%%%%%%%%%%%%%%%
 S_v = 2000; % (mS/cm^2) % stimulus value
 S_T0 = 1; % (ms) start time of when stimulus is added
-S_T1 = 1.1; % (ms) end time of when stimulus is added
-S_P0 = 0.0001; % (cm) start position of adding the stimulus
-S_P1 = 0.0004; % (cm) end position of adding the stimulus
+S_T1 = 5; % (ms) end time of when stimulus is added
+S_P0 = 0.00005; % (cm) start position of adding the stimulus
+S_P1 = 0.00045; % (cm) end position of adding the stimulus
 % in the S functi on ii, is the space index and tt is the time index
 S = @(ii, tt) S_v * ((abs(tt * dt - S_T0) <= 1e-10 | tt * dt > S_T0) & ...
                     (tt * dt < S_T1 | abs(tt * dt - S_T1) <= 1e-10) & ...
@@ -516,4 +516,4 @@ legend(legendStrings3, 'Interpreter','latex')
 ylabel("Probabilities of ion channels opening/closing.")
 xlabel("Time in milliseconds.")
 
-save('DC_Huang_TubeParalyene_stim_2000_rpn_100fold.mat');
+save('DC_Huang_TubeParalyene_higher_stim_rpn_10fold.mat');
